e725914a2102493444863ed1ebbd2d7c711283a9
mcabrerapf <mcabrerapf@gmail.com>
2023-03-06 19:04:14 +0100

    added aws auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "weekmenu2aedb9fc": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
 import { Main, Footer } from './Components';
 import { MainContextWrapper } from './Context';
 
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+
 function App() {
   return (
-    <MainContextWrapper>
-      <div className="app">
-        <Main />
-        <Footer />
-      </div>
-    </MainContextWrapper>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <MainContextWrapper>
+          <div className="app">
+            <button type="button" onClick={signOut}>get me out</button>
+            {console.log(user)}
+            <Main />
+            <Footer />
+          </div>
+        </MainContextWrapper>
+      )}
+    </Authenticator>
 
   );
 }
