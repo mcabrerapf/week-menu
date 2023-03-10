59b1091d52bb6881a4370c2b191c34e1236ac285
Manuel Cabrera <manuel@Manuels-MacBook-Pro-2.local>
2023-03-10 18:39:29 +0100

    added copy shoping lsit ingredients

--- src/Components/WeekView/ShopingList/ShopingListNote.jsx
+++ src/Components/WeekView/ShopingList/ShopingListNote.jsx
@@ -2,13 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ShopingList.css';
 import ShopingListNoteSection from './ShopingListNoteSection';
+import { useLongPress } from '../../../Hooks';
 
 function ShopingListNote({ ingredients, hidden }) {
+  const handleCopyShopingList = () => {
+    const shopingListItems = [];
+    Object.keys(ingredients).forEach((sectionKey) => {
+      const sectionData = ingredients[sectionKey];
+      sectionData.forEach((data) => {
+        const { name, quantity } = data;
+        shopingListItems.push(`${name}: ${quantity}`);
+      });
+    });
+    navigator.clipboard.writeText(JSON.stringify(shopingListItems.join(', ')));
+  };
+  const longPressProps = useLongPress({ onLongPress: () => handleCopyShopingList() });
+
   if (!ingredients) return null;
 
   return (
     <div className="shoppin-list-note-container">
-      <div className="shoping-list-note" style={{ display: hidden ? 'none' : 'block' }}>
+      <div
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...longPressProps}
+        className="shoping-list-note"
+        style={{ display: hidden ? 'none' : 'block' }}
+      >
         {Object.keys(ingredients).map((sectionKey) => {
           const sectionData = ingredients[sectionKey];
           return (
