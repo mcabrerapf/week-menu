30fc7bb20b4a4a33986607f3eca0cba016de37fd
Manuel Guest <manuelguest@Manuels-MacBook-Pro-2.local>
2024-02-18 12:57:05 +0100

    improved text search

--- src/Components/List/helpers.js
+++ src/Components/List/helpers.js
@@ -24,15 +24,18 @@ const filterList = (list, searchValue, filterValue) => {
   if (!searchValue && !filterValue) return list;
   const useFavourite = filterValue === 'favourite';
   return list.filter(({
-    name, type, types, favourite,
+    name, ingredients = [], type, types, favourite,
   }) => {
     const filterMatch = types ? types.includes(filterValue) : type === filterValue;
     const matchToUse = useFavourite ? favourite : filterMatch;
     if (searchValue) {
+      const hasTextMatch = name.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1
+      || !!ingredients
+        .find(({ name: iname }) => iname.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1);
       if (filterValue) {
-        return name.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1 && matchToUse;
+        return hasTextMatch && matchToUse;
       }
-      return name.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1;
+      return hasTextMatch;
     }
     if (filterValue) return matchToUse;
     return true;
